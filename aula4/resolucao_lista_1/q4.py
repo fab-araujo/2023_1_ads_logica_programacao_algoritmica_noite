490342da5318466497c7c5ca821600d64d51d248
fabricioaraujo <faa@ufpa.br>
2023-03-16 19:48:55 -0300

    Create q4.py

---
+++ aula4/resolucao_lista_1/q4.py
@@ -0,0 +1,16 @@
+"""
+4.	Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+"""
+n1 = input("Digite a primeira nota: ")
+n2 = input("Digite a segunda nota: ")
+n3 = input("Digite a terceira nota: ")
+n4 = input("Digite a quarta nota: ")
+
+n1 = int(n1)
+n2 = int(n2)
+n3 = int(n3)
+n4 = int(n4)
+
+media = (n1 + n2 + n3 + n4) / 4
+
+print(media)
